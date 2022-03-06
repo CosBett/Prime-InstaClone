8e5699952818cae4518ed45f0aa5deefd5eb47bc
CosBett <cosmasbett9@gmail.com>
2022-03-06 10:50:28 +0300

    update view function for rendering landing page

--- post/views.py
+++ post/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    landing_page = landing_page
-    return render(request, 'landing_page.html', landing_page) 
+    
+    return render(request, 'landing_page.html') 
