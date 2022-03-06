1d940ef7e7fdf0d5f70ceb31899786c024fa4b4c
CosBett <cosmasbett9@gmail.com>
2022-03-06 07:26:37 +0300

    register install apps

--- instaClone/settings.py
+++ instaClone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6tclb_9e7$*fzqeom5*qh*=2maq%zpef+o68c^ppg@oa+)ovtr'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth'
+    'user_profile'
+    'post'
 ]
 
 MIDDLEWARE = [
