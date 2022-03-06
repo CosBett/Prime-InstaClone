6b0c4394180618e23ce8dd34add6be1b46f853de
CosBett <cosmasbett9@gmail.com>
2022-03-06 10:37:54 +0300

    urls paths configuration

--- post/views.py
+++ post/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    landing_page = landing_page
+    return render(request, 'landing_page.html', landing_page) 
