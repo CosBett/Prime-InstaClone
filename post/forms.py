e3eab3e725ed5883c54c83395d126419210e9dee
CosBett <cosmasbett9@gmail.com>
2022-03-08 11:29:54 +0300

    declare user_profile model

---
+++ post/forms.py
@@ -0,0 +1,9 @@
+import email
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_lenght=300, help_text= 'Required. Inform a valid email address'),
+    class Meta:
+      model = User
+      fields
--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class User_profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(blank=True, max_length=100)
+    profile_picture = models.ImageField(upload_to='pictures/', default='default.png')
+    bio = models.TextField(max_length=1500, default="My Bio", blank=True)
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} User_profile'
