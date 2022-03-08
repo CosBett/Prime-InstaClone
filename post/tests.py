f7f34958e4e6c9a25c7f2905a093ad3219690933
CosBett <cosmasbett9@gmail.com>
2022-03-08 11:42:11 +0300

    createsetUp test code for User_profile

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import User_profile
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+
+class TestUser_profile(TestCase):
+
+    def setUp(self):
+        self.user = User(username='Prime')
+        self.user.save()
+        self.profile_test = User_profile(id=1, name='image', profile_picture='default.png', bio='this is a test User profile',
+                                    user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_test, User_profile))
