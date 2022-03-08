85a6b98b3900ece677e4cf6994b10c0e283d23e9
CosBett <cosmasbett9@gmail.com>
2022-03-08 11:33:17 +0300

    create post , save method for user_profile

--- post/models.py
+++ post/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
 class User_profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
@@ -10,3 +12,11 @@ class User_profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} User_profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            User_profile.objects.create(user=instance)
+
+   
+   
