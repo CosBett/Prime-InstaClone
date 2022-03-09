4702dc309966002fc6ac56b8c969f8715d65da0a
CosBett <cosmasbett9@gmail.com>
2022-03-09 13:10:46 +0300

    create form class for comments  module class

--- post/forms.py
+++ post/forms.py
@@ -27,8 +27,17 @@ class Update_UserForm(forms.ModelForm):
       fields = ('username', 'email')
 
 class PostForm(forms.ModelForm):
-  
+
     class Meta:
         model = Post
         fields = ('image', 'caption')
 
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment here...'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)
