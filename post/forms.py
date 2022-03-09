1a3a497e19de271743f62b645a16efff8908b833
CosBett <cosmasbett9@gmail.com>
2022-03-09 13:08:56 +0300

    create form class for Post module class

--- post/forms.py
+++ post/forms.py
@@ -7,13 +7,28 @@ from .models import User_profile, Post, Comment
 # Account creation form. 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_lenght=300, help_text= 'Required. Input a valid email address')
+    
     class Meta:
       model = User
       fields = ('username', 'email', 'password1', 'password2')
 
 
 class Update_profileForm(forms.ModelForm):
+
     class Meta:
       model = User_profile
       fields = ('name', 'profile_picture', 'bio', 'location')
+
+class Update_UserForm(forms.ModelForm):
+    email = forms.EmailField(max_lenght=300, help_text= 'Required. Input a valid email address')
+    
+    class Meta:
+      model = User
+      fields = ('username', 'email')
+
+class PostForm(forms.ModelForm):
+  
+    class Meta:
+        model = Post
+        fields = ('image', 'caption')
 
