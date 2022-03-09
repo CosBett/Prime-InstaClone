840c869e0b200e7ea5d65d967c9ee4b00e9a0adf
CosBett <cosmasbett9@gmail.com>
2022-03-09 14:42:03 +0300

    create admin superuser

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User_profile, Post, Comment, Follow
 # Register your models here.
+
+admin.site.register(User_profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Follow)
