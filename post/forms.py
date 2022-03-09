7da05f66251d96ba8041fb5487b08e113715e944
CosBett <cosmasbett9@gmail.com>
2022-03-09 13:05:28 +0300

    create class for updating user profile

--- post/forms.py
+++ post/forms.py
@@ -1,4 +1,4 @@
-import email
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -6,8 +6,14 @@ from .models import User_profile, Post, Comment
 
 # Account creation form. 
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(max_lenght=300, help_text= 'Required. Inform a valid email address'),
+    email = forms.EmailField(max_lenght=300, help_text= 'Required. Input a valid email address')
     class Meta:
       model = User
       fields = ('username', 'email', 'password1', 'password2')
+
+
+class Update_profileForm(forms.ModelForm):
+    class Meta:
+      model = User_profile
+      fields = ('name', 'profile_picture', 'bio', 'location')
 
