d15759c99cb2de252e5e5e928baf245d5dd20338
CosBett <cosmasbett9@gmail.com>
2022-03-16 02:33:38 +0300

    edit landing page endpoint

--- post/urls.py
+++ post/urls.py
@@ -1,10 +1,10 @@
-from django import views
-from django.contrib import admin
 from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.index, name = 'landing_page' ),
+    path('',views.landing_page, name = 'landing_page' ),
     path('signup/',views.sign_up, name = 'signup' ),
+    path('account/', include('django.contrib.auth.urls')),
+
 
 ]
--- post/views.py
+++ post/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render,redirect
 from .forms import SignupForm, Update_profileForm, Update_UserForm, PostForm, CommentForm
 from .models import User_profile,Post, Comment, Follow
 from django.contrib.auth import authenticate, login
-# Create your views here.
-def index(request):
-     form = SignupForm()
-     landing_context = {'form': form} 
-     return render(request,'landing_page/landing_page.html',landing_context) 
+from django.contrib.auth.decorators import login_required
+
+# View function for homepage and login 
+
+def landing_page(request):
+     
+     return render(request,'landing_page/landing_page.html') 
 
 # view function for account creation.    
 def sign_up(request):
@@ -15,7 +17,7 @@ def sign_up(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_date.get('password1')
+            raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password= raw_password)
             login(request, user)
             
