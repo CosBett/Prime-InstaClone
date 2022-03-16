25a977ddd6ddb0e98102785728a2000f9c2cf779
CosBett <cosmasbett9@gmail.com>
2022-03-16 03:03:21 +0300

    create view function for homepage and its endpoint

--- post/views.py
+++ post/views.py
@@ -1,14 +1,33 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect
 from .forms import SignupForm, Update_profileForm, Update_UserForm, PostForm, CommentForm
 from .models import User_profile,Post, Comment, Follow
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # View function for homepage and login 
 
 def landing_page(request):
      
      return render(request,'landing_page/landing_page.html') 
+
+@login_required(login_url='login')
+def index(request):
+    images = Post.objects.all()
+    users = User.objects.exclude(id=request.user.id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.profile
+            post.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = PostForm()
+    index_context = {'images': images,'form': form,'users': users}
+
+    return render(request, 'index.html', index_context)
 
 # view function for account creation.    
 def sign_up(request):
